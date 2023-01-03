a001165ed308ef8ca51d9dda718169cbda499fdb
John Doe <mangalkunchalwar9999@gmail.com>
2023-01-03 21:09:12 +0530

    welcome to Practice Problem of LinkedList

---
+++ Main.java
@@ -0,0 +1,7 @@
+package assignment14.practiceproblemLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("welcome to Practice Problem of LinkedList");
+    }
+}
